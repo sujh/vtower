e86bb81cbf7c739fd566273dbf8a93db8e4796a9
sujunhao <spike76@qq.com>
2020-01-16 18:04:46 +0800

    devise 登录注册

--- app/javascript/home/index.vue
+++ app/javascript/home/index.vue
@@ -1,14 +1,45 @@
 <template>
   <div id="app">
-    <ol>
-      <li v-for="todo in todos">
-        <label>
-          <input type="checkbox" v-on:change="toggle(todo)" v-bind:checked="todo.done">
-          <del v-if="todo.done">{{todo.text}}</del>
-          <span v-else>{{todo.text}}</span>
-        </label>
-      </li>
-    </ol>
+    <el-container>
+      <el-aside width="400px">
+        <h1>VTower</h1>
+        <p>A rails app for practicing Vue, using vue & webpack</p>
+      </el-aside>
+      <el-main>
+        <el-form label-width="80px" action="/users/sign_in" method="post" v-if="login_form">
+          <el-form-item label="邮箱">
+            <el-input name="user[email]" v-model="user.email"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input show-password name="user[password]" v-model="user.password"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" native-type="submit" size="small">登  录</el-button>
+            <div style="float: right">
+              <el-link @click="login_form=!login_form" type="primary">没有账号？点击注册</el-link>
+            </div>
+          </el-form-item>
+        </el-form>
+
+        <el-form label-width="80px" action="/users" method="post" v-else>
+          <el-form-item label="邮箱">
+            <el-input name="user[email]" v-model="user.email"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input show-password name="user[password]" v-model="user.password"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码">
+            <el-input show-password name="user[password_confirmation]" v-model="user.password_confirmation"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" native-type="submit">注册</el-button>
+            <div style="float: right">
+              <el-link @click="login_form=!login_form" type="primary">返回登录</el-link>
+            </div>
+          </el-form-item>
+        </el-form>
+      </el-main>
+    </el-container>
   </div>
 </template>
 
@@ -16,46 +47,38 @@
   export default {
     data() {
       return {
-        todos: [
-          {text: "Learn ", done: false},
-          {text: "Learn Vue", done: false},
-          {text: "Play around in JSFiddle", done: true},
-          {text: "Build something awesome", done: true}
-        ]
+        login_form: true,
+        user: {
+          email: '',
+          password: '',
+          password_confirmation: ''
+        }
       }
     },
-    mounted(){console.log('mmm', this.$el)},
-    methods: {
-      toggle: function(todo){
-        todo.done = !todo.done
-      }
-    }
+    mounted(){
+      this.$nextTick(() =>{
+        let token = document.querySelector('meta[name="csrf-token"]').content
+        let ele = document.createElement('input')
+        Object.assign(ele, {name: 'authenticity_token', value: token, type: 'hidden'})
+        document.querySelector('form.el-form').appendChild(ele)
+      })
+    },
   }
 </script>
 
-<style>
-  body {
-    padding: 20px;
-    font-family: Helvetica;
+<style scoped>
+  h1 {
+    font-size: 60px;
+    margin: 30px 0;
   }
-
-  #app {
-    background: #fff;
-    border-radius: 4px;
-    padding: 20px;
-    transition: all 0.2s;
+  .el-container {
+    margin: 40px 200px
   }
-
-  li {
-    margin: 8px 0;
+  .el-aside {
+    margin: 0 50px
   }
-
-  h2 {
-    font-weight: bold;
-    margin-bottom: 15px;
-  }
-
-  del {
-    color: rgba(0, 0, 0, 0.3);
+  .el-main {
+    min-width: 300px;
+    max-width: 600px;
   }
 </style>
--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,5 +1,9 @@
-import Vue from 'vue/dist/vue.esm';
-import Home from '../home';
+import Vue from 'vue/dist/vue.esm'
+import Home from '../home'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = new Vue({
